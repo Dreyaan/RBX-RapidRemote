5950d23e75ef45e74f6e973a2c79e7eb3b079c4b
Adrian <79951121+Dreyaan@users.noreply.github.com>
2023-02-18 17:06:49 +0700

    Create RapidRemotes.lua

---
+++ RapidRemotes.lua
@@ -0,0 +1,24 @@
+local settings = {repeatamount = 4} --Amount of repeat
+
+local gm = getrawmetatable(game)
+setreadonly(gm, false)
+local namecall = gm.__namecall
+gm.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    local args = {...}
+    if method == "FireServer" or method == "InvokeServer" then
+		if tostring(self) == "Hit" then --Specify remote to duplicate
+			for i = 1,settings.repeatamount do
+				namecall(self, unpack(args)) --Start repeating
+			end
+		end
+    end
+    return namecall(self, ...)
+end)
+setreadonly(gm, true)
+print("Duplicated!")
+
+-- Known remotes --
+--Fishing sim {MonsterHit}
+-- All of us are dead {WeaponHit}
+-- WestBound {Hit, Inventory, SkinAnimal}
